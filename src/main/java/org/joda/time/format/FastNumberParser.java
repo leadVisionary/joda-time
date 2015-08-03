5f031f62fa8bbfee60b980935a30f3f10327514e
Vaidyanathan <vaidyann@amazon.com>
2015-08-02 17:42:44 -0700

    applied https://sourcemaking.com/refactoring/replace-method-with-method-object
    apply https://sourcemaking.com/refactoring/extract-class to minimize the scope of things under concern.

---
+++ src/main/java/org/joda/time/format/FastNumberParser.java
@@ -0,0 +1,97 @@
+package org.joda.time.format;
+
+import org.joda.time.DateTimeFieldType;
+import org.joda.time.format.DateTimeParserBucket;
+
+final class FastNumberParser {
+    private final DateTimeFieldType iFieldType;
+    private final int iMaxParsedDigits;
+    private final boolean iSigned;
+
+    public FastNumberParser(final int maxParsedDigits,
+                            final boolean signed,
+                            final DateTimeFieldType fieldType) {
+        iFieldType = fieldType;
+        iMaxParsedDigits = maxParsedDigits;
+        iSigned = signed;
+    }
+
+    public int parse(final DateTimeParserBucket bucket,
+                     final CharSequence text,
+                     int position) {
+        int limit = Math.min(iMaxParsedDigits, text.length() - position);
+
+        boolean negative = false;
+        int length = 0;
+        while (length < limit) {
+            char c = text.charAt(position + length);
+            final boolean isFirstCharacterOperator = length == 0 && (c == '-' || c == '+');
+            if (isFirstCharacterOperator && iSigned) {
+                negative = c == '-';
+
+                if (isPastBoundaryOrNotDigit(text, position, limit, length)) {
+                    break;
+                }
+
+                if (negative) {
+                    length++;
+                } else {
+                    // Skip the '+' for parseInt to succeed.
+                    position++;
+                }
+                // Expand the limit to disregard the sign character.
+                limit = Math.min(limit + 1, text.length() - position);
+                continue;
+            }
+            if (isNotADigit(c)) {
+                break;
+            }
+            length++;
+        }
+
+        if (length == 0) {
+            return ~position;
+        }
+
+        int value;
+        if (length >= 9) {
+            // Since value may exceed integer limits, use stock parser
+            // which checks for this.
+            value = Integer.parseInt(text.subSequence(position, position += length).toString());
+        } else {
+            int i = position;
+            if (negative) {
+                i++;
+            }
+            try {
+                value = text.charAt(i++) - '0';
+            } catch (StringIndexOutOfBoundsException e) {
+                return ~position;
+            }
+            position += length;
+            while (i < position) {
+                value = ((value << 3) + (value << 1)) + text.charAt(i++) - '0';
+            }
+            if (negative) {
+                value = -value;
+            }
+        }
+
+        bucket.saveField(iFieldType, value);
+        return position;
+    }
+
+    private static boolean isPastBoundaryOrNotDigit(final CharSequence text, final int position, final int limit, final int length) {
+        final boolean isPastBoundary = length + 1 >= limit;
+        if (isPastBoundary) {
+            return true;
+        }
+        final char nextCharacter = text.charAt(position + length + 1);
+        final boolean isNotDigit = isNotADigit(nextCharacter);
+        return isNotDigit;
+    }
+
+    private static boolean isNotADigit(final char c) {
+        return c < '0' || c > '9';
+    }
+}
