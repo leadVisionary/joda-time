5e3a9d27ca0c05a17b1466e72fc8032884891ec3
Vaidyanathan <vaidyann@amazon.com>
2015-08-02 17:42:44 -0700

    applied https://sourcemaking.com/refactoring/replace-method-with-method-object

--- src/main/java/org/joda/time/format/DateTimeFormatterBuilder.java
+++ src/main/java/org/joda/time/format/DateTimeFormatterBuilder.java
@@ -1301,75 +1301,94 @@ public class DateTimeFormatterBuilder {
         }
 
         public int parseInto(DateTimeParserBucket bucket, CharSequence text, int position) {
-            int limit = Math.min(iMaxParsedDigits, text.length() - position);
-
-            boolean negative = false;
-            int length = 0;
-            while (length < limit) {
-                char c = text.charAt(position + length);
-                final boolean isFirstCharacterOperator = length == 0 && (c == '-' || c == '+');
-                if (isFirstCharacterOperator && iSigned) {
-                    negative = c == '-';
-
-                    if (isPastBoundaryOrNotDigit(text, position, limit, length))
-                    {
-                        break;
-                    }
-
-                    if (negative) {
-                        length++;
-                    } else {
-                        // Skip the '+' for parseInt to succeed.
-                        position++;
-                    }
-                    // Expand the limit to disregard the sign character.
-                    limit = Math.min(limit + 1, text.length() - position);
-                    continue;
-                }
-                if (isNotADigit(c)) {
-                    break;
-                }
-                length++;
-            }
-
-            if (length == 0) {
-                return ~position;
-            }
-
-            int value;
-            if (length >= 9) {
-                // Since value may exceed integer limits, use stock parser
-                // which checks for this.
-                value = Integer.parseInt(text.subSequence(position, position += length).toString());
-            } else {
-                int i = position;
-                if (negative) {
-                    i++;
-                }
-                try {
-                    value = text.charAt(i++) - '0';
-                } catch (StringIndexOutOfBoundsException e) {
-                    return ~position;
-                }
-                position += length;
-                while (i < position) {
-                    value = ((value << 3) + (value << 1)) + text.charAt(i++) - '0';
-                }
-                if (negative) {
-                    value = -value;
-                }
-            }
-
-            bucket.saveField(iFieldType, value);
-            return position;
+            return new FastNumberParser(bucket, text, position).invoke(iMaxParsedDigits, iSigned, iFieldType);
         }
 
-        private static boolean isPastBoundaryOrNotDigit(final CharSequence text, final int position, final int limit, final int length) {
-            final boolean isPastBoundary = length + 1 >= limit;
-            if (isPastBoundary) { return true; }
-            final char nextCharacter = text.charAt(position + length + 1);
-            final boolean isNotDigit = isNotADigit(nextCharacter);
-            return isNotDigit;
+        private static class FastNumberParser {
+            private final DateTimeParserBucket bucket;
+            private final CharSequence text;
+            private int position;
+
+            public FastNumberParser(final DateTimeParserBucket bucket,
+                                    final CharSequence text,
+                                    final int position) {
+                this.bucket = bucket;
+                this.text = text;
+                this.position = position;
+            }
+
+            private static boolean isPastBoundaryOrNotDigit(final CharSequence text, final int position, final int limit, final int length) {
+                final boolean isPastBoundary = length + 1 >= limit;
+                if (isPastBoundary) { return true; }
+                final char nextCharacter = text.charAt(position + length + 1);
+                final boolean isNotDigit = isNotADigit(nextCharacter);
+                return isNotDigit;
+            }
+
+            public int invoke(final int iMaxParsedDigits, final boolean iSigned, final DateTimeFieldType iFieldType) {
+                final int remainingCharacters = text.length() - position;
+                int limit = Math.min(iMaxParsedDigits, remainingCharacters);
+
+                boolean negative = false;
+                int length = 0;
+                while (length < limit) {
+                    char c = text.charAt(position + length);
+                    final boolean isFirstCharacterOperator = length == 0 && (c == '-' || c == '+');
+                    if (isFirstCharacterOperator && iSigned) {
+                        negative = c == '-';
+
+                        if (isPastBoundaryOrNotDigit(text, position, limit, length))
+                        {
+                            break;
+                        }
+
+                        if (negative) {
+                            length++;
+                        } else {
+                            // Skip the '+' for parseInt to succeed.
+                            position++;
+                        }
+                        // Expand the limit to disregard the sign character.
+                        limit = Math.min(limit + 1, remainingCharacters);
+                        continue;
+                    }
+                    if (isNotADigit(c)) {
+                        break;
+                    }
+                    length++;
+                }
+
+                if (length == 0) {
+                    return ~position;
+                }
+
+                int value;
+                if (length >= 9) {
+                    // Since value may exceed integer limits, use stock parser
+                    // which checks for this.
+                    value = Integer.parseInt(text.subSequence(position, position += length).toString());
+                } else {
+                    int i = position;
+                    if (negative) {
+                        i++;
+                    }
+                    try {
+                        value = text.charAt(i++) - '0';
+                    } catch (StringIndexOutOfBoundsException e) {
+                        return ~position;
+                    }
+                    position += length;
+                    while (i < position) {
+                        value = ((value << 3) + (value << 1)) + text.charAt(i++) - '0';
+                    }
+                    if (negative) {
+                        value = -value;
+                    }
+                }
+
+                bucket.saveField(iFieldType, value);
+                return position;
+            }
         }
 
         private static boolean isNotADigit(final char c) {
