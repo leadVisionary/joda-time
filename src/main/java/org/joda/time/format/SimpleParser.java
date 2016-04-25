f78249f0a9017519b3b10fbe58ea69fdf1217cad
V <nvaidyanathan@whitepages.com>
2016-04-24 23:25:26 -0700

    apply EXTRACT METHOD to identify duplication in methods.

--- src/main/java/org/joda/time/format/SimpleParser.java
+++ src/main/java/org/joda/time/format/SimpleParser.java
@@ -20,12 +20,16 @@ final class SimpleParser {
         int newPos = parser.parseInto(bucket, text, 0);
         if (newPos >= 0) {
             if (newPos >= text.length()) {
-                return bucket.computeMillis(true, text);
+                return getMillis(text, bucket);
             }
         } else {
             newPos = ~newPos;
         }
         throw new IllegalArgumentException(FormatUtils.createErrorMessage(text.toString(), newPos));
+    }
+
+    private static long getMillis(CharSequence text, DateTimeParserBucket bucket) {
+        return bucket.computeMillis(true, text);
     }
 
     static LocalDateTime parseLocalDateTime(CharSequence text, InternalParser parser, DateTimeParserBucket bucket) {
@@ -36,21 +40,25 @@ final class SimpleParser {
         int newPos = parser.parseInto(bucket, text, 0);
         if (newPos >= 0) {
             if (newPos >= text.length()) {
-                long millis = bucket.computeMillis(true, text);
-                Chronology chrono = bucket.getChronology();
-                if (bucket.getOffsetInteger() != null) {  // treat withOffsetParsed() as being true
-                    int parsedOffset = bucket.getOffsetInteger();
-                    DateTimeZone parsedZone = DateTimeZone.forOffsetMillis(parsedOffset);
-                    chrono = chrono.withZone(parsedZone);
-                } else if (bucket.getZone() != null) {
-                    chrono = chrono.withZone(bucket.getZone());
-                }
-                return new LocalDateTime(millis, chrono);
+                return getLocalDateTime(text, bucket);
             }
         } else {
             newPos = ~newPos;
         }
         throw new IllegalArgumentException(FormatUtils.createErrorMessage(text.toString(), newPos));
+    }
+
+    private static LocalDateTime getLocalDateTime(CharSequence text, DateTimeParserBucket bucket) {
+        long millis = getMillis(text, bucket);
+        Chronology chrono = bucket.getChronology();
+        if (bucket.getOffsetInteger() != null) {  // treat withOffsetParsed() as being true
+            int parsedOffset = bucket.getOffsetInteger();
+            DateTimeZone parsedZone = DateTimeZone.forOffsetMillis(parsedOffset);
+            chrono = chrono.withZone(parsedZone);
+        } else if (bucket.getZone() != null) {
+            chrono = chrono.withZone(bucket.getZone());
+        }
+        return new LocalDateTime(millis, chrono);
     }
 
     static DateTime parseDateTime(boolean iOffsetParsed, DateTimeZone iZone, String text, InternalParser parser, DateTimeParserBucket bucket) {
@@ -60,16 +68,20 @@ final class SimpleParser {
         int newPos = parser.parseInto(bucket, text, 0);
         if (newPos >= 0) {
             if (newPos >= text.length()) {
-                DateTime dt = new DateTime(bucket.computeMillis(true, text), bucket.getBucketChronology(iOffsetParsed));
-                if (iZone != null) {
-                    dt = dt.withZone(iZone);
-                }
-                return dt;
+                return getDateTime(iOffsetParsed, iZone, text, bucket);
             }
         } else {
             newPos = ~newPos;
         }
         throw new IllegalArgumentException(FormatUtils.createErrorMessage(text, newPos));
+    }
+
+    private static DateTime getDateTime(boolean iOffsetParsed, DateTimeZone iZone, String text, DateTimeParserBucket bucket) {
+        DateTime dt = new DateTime(bucket.computeMillis(true, text), bucket.getBucketChronology(iOffsetParsed));
+        if (iZone != null) {
+            dt = dt.withZone(iZone);
+        }
+        return dt;
     }
 
     static MutableDateTime parseMutableDateTime(boolean iOffsetParsed, DateTimeZone iZone, String text, InternalParser parser, DateTimeParserBucket bucket) {
@@ -80,15 +92,19 @@ final class SimpleParser {
         int newPos = parser.parseInto(bucket, text, 0);
         if (newPos >= 0) {
             if (newPos >= text.length()) {
-                MutableDateTime dt = new MutableDateTime(bucket.computeMillis(true, text), bucket.getBucketChronology(iOffsetParsed));
-                if (iZone != null) {
-                    dt.setZone(iZone);
-                }
-                return dt;
+                return getMutableDateTime(iOffsetParsed, iZone, text, bucket);
             }
         } else {
             newPos = ~newPos;
         }
         throw new IllegalArgumentException(FormatUtils.createErrorMessage(text, newPos));
+    }
+
+    private static MutableDateTime getMutableDateTime(boolean iOffsetParsed, DateTimeZone iZone, String text, DateTimeParserBucket bucket) {
+        MutableDateTime dt = new MutableDateTime(bucket.computeMillis(true, text), bucket.getBucketChronology(iOffsetParsed));
+        if (iZone != null) {
+            dt.setZone(iZone);
+        }
+        return dt;
     }
 }
