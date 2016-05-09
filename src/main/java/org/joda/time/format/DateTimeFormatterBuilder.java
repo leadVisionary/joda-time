544d009361e48fef44b501ba61482d503646314f
V <nvaidyanathan@whitepages.com>
2016-05-08 17:14:46 -0700

    remove OffsetCalculator and move calculate() back to NumberFormatter.

--- src/main/java/org/joda/time/format/DateTimeFormatterBuilder.java
+++ src/main/java/org/joda/time/format/DateTimeFormatterBuilder.java
@@ -1302,8 +1302,38 @@ public class DateTimeFormatterBuilder {
 
         public int parseInto(DateTimeParserBucket bucket, CharSequence text, int position) {
             final NumericSequence sequence = new NumericSequence(text, iMaxParsedDigits, iSigned, position);
-            bucket.saveField(iFieldType, OffsetCalculator.calculate(sequence));
+            bucket.saveField(iFieldType, calculate(sequence));
             return sequence.getCurrentPosition();
+        }
+
+        static int calculate(final NumericSequence sequence) {
+            final int length = sequence.getLength();
+            if (length == 0 || !sequence.hasMoreThanOneDigit()) {
+                sequence.setCurrentPosition(~sequence.getCurrentPosition());
+                return 0;
+            } else if (length >= 9) {
+                return defaultCalculate(sequence, length);
+            } else {
+                return fastCalculate(sequence, length);
+            }
+        }
+
+        private static int defaultCalculate(final NumericSequence sequence, int length) {
+            // Since value may exceed integer limits, use stock parser
+            // which checks for this.
+            final String toParse = sequence.getPart(length);
+            sequence.setCurrentPosition(sequence.getCurrentPosition() + length);
+            return Integer.parseInt(toParse);
+        }
+
+        private static int fastCalculate(final NumericSequence sequence, int length) {
+            int startingIndex = sequence.isNegative() ? sequence.getCurrentPosition() + 1 : sequence.getCurrentPosition();
+            int calculated = sequence.getAsciiCharacterFor(startingIndex);
+            sequence.setCurrentPosition(sequence.getCurrentPosition() + length);
+            for (int i = startingIndex + 1; i < sequence.getCurrentPosition(); i++) {
+                calculated = ((calculated << 3) + (calculated << 1)) + sequence.getAsciiCharacterFor(i);
+            }
+            return sequence.isNegative() ? -calculated : calculated;
         }
 
     }
