f512640e075b22905b1b557e271d9e8fd4e17a16
Vaidyanathan <vaidyann@amazon.com>
2017-06-25 14:35:41 -0700

    revert to pre-study state.

--- src/main/java/org/joda/time/format/parsing/NumericSequence.java
+++ src/main/java/org/joda/time/format/parsing/NumericSequence.java
@@ -13,7 +13,7 @@ public final class NumericSequence {
         this.text = text;
         startsWithSign = isPrefixedWithPlusOrMinus(maximumDigitsToParse, isSigned, startingPosition);
         negative = startsWithSign && text.charAt(startingPosition) == '-';
-        currentPosition = startingPosition;
+        currentPosition = startsWithSign ? (negative) ? startingPosition : startingPosition + 1 : startingPosition;
         limit = findLastIndexOfStringToParse(text, maximumDigitsToParse);
         length = calculateLength();
     }
@@ -64,7 +64,7 @@ public final class NumericSequence {
     }
 
     private int getIndexOfFirstDigit() {
-        return startsWithSign ? getCurrentPosition() + 1 : getCurrentPosition();
+        return negative ? getCurrentPosition() + 1 : getCurrentPosition();
     }
 
     private int handleFailure() {
